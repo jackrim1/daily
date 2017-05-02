496436c30bafe889dc5c5364f0f56988ec5db936
Jackrim1 <jriminton@gmail.com>
2017-05-02 15:52:15 +0100

    can now add descriptions

--- db/migrate/20170501210441_create_records.rb
+++ db/migrate/20170501210441_create_records.rb
@@ -2,6 +2,7 @@ class CreateRecords < ActiveRecord::Migration[5.0]
   def change
     create_table :records do |t|
       t.string :title
+      t.string :description
       t.references :user, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,10 @@ ActiveRecord::Schema.define(version: 20170501211936) do
 
   create_table "records", force: :cascade do |t|
     t.string   "title"
+    t.string   "description"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_records_on_user_id"
   end
 
